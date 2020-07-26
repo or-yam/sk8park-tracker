c7b5946d0250dc9171c2d3691abbcb29753796e0
Oryam <48219965+or-yam@users.noreply.github.com>
2020-07-26 13:50:43 +0300

    added park and user model

---
+++ dist/models/ParkManager.js
@@ -0,0 +1,24 @@
+export class ParkManager {
+  constructor() {
+    this._data = { skateParks: [] };
+  }
+
+  getAllParks = async (user) => {
+    const isGuest = user.guest;
+    let data = await $.get(`api/parks/${isGuest}`);
+    this._data.skateParks = data;
+  };
+
+  addPark = async (park) => {
+    const newPark = await $.post('api/parks');
+    this._data.skateParks.push(newPark);
+  };
+
+  giveRating = async (rate) => {
+    await $.ajax({
+      url: `api/parks/${rate}`,
+      type: 'PUT',
+    });
+    console.log('updated');
+  };
+}

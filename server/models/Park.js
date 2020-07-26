45c3ea4a849b784994825667fcc0c32869c0e448
MichalSagi <smitzi@gmail.com>
2020-07-26 14:30:54 +0300

    geting data from api

--- server/models/Park.js
+++ server/models/Park.js
@@ -1,12 +1,23 @@
 const mongoose = require('mongoose');
-
 const Schema = mongoose.Schema;
 
 const parkSchema = new Schema({
-//
-//
+    lat: Number,
+    lng: Number,
+    parkImg : String,
+    name: String,
+    style: [{
+        street: Boolean,
+        vert: Boolean,
+        pump:Boolean 
+      }],
+    activityHours: String,
+    createdBy: {type:Schema.Types.ObjectId, ref:'User'},
+    rating: Number,
+    about: String,
+    defult: Boolean
 });
 
-const dummy = mongoose.model('///', parkSchema);
+const Park = mongoose.model('Park', parkSchema);
 
-module.exports = dummy;
+module.exports = Park;
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,9 +1,33 @@
-const express = require('express');
-const moment = require('moment');
-//const MongooseModel = require('../models/MongooseModel');
+const express = require("express");
+// const moment = require("moment");
+// const Park = require("./models/");
+// const User = require("./models/")
+const axios = require("axios");
 
 const router = express.Router();
 
-//Routes
+const getDataFromGoogle = () => {
+  const API_KEY = "AIzaSyDayIjgDn311gKQQSC1ElzUeRw3Ww7Ixu4";
+  const radius = 300000;
+  const lat = 31.7683;
+  const lng = 35.2137;
+  const URL = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=${radius}&type=skatebord&keyword=skate&key=${API_KEY}`;
+  return axios.get(URL);
+ 
+};
+
+router.get("/api/skateparks", function(req, res) {
+    getDataFromGoogle().then(data => {
+    let filterd = data.data.results.map(p => 
+        ({lat :p.geometry.location.lat,
+        lng : p.geometry.location.lng,
+        name : p.name})
+      );
+      res.send(filterd);
+  });
+});
+
+
+
 
 module.exports = router;

b2f4a5346276489dbf2de889bee0c7b1952195fc
Oryam <48219965+or-yam@users.noreply.github.com>
2021-04-27 21:22:15 +0300

    Refactor, ParkManager.

--- dist/models/ParkManager.js
+++ dist/models/ParkManager.js
@@ -3,47 +3,27 @@ export class ParkManager {
     this._data = { skateParks: [], tempPark: {} };
   }
 
-  calculateRating = (ratingObj) => {
-    const sum =
-      ratingObj.one +
-      ratingObj.two +
-      ratingObj.three +
-      ratingObj.four +
-      ratingObj.five;
-    const multi =
-      ratingObj.one * 1 +
-      ratingObj.two * 2 +
-      ratingObj.three * 3 +
-      ratingObj.four * 4 +
-      ratingObj.five * 5;
+  calculateRating = ({ one, two, three, four, five }) => {
+    const sum = one + two + three + four + five;
+    const multi = one * 1 + two * 2 + three * 3 + four * 4 + five * 5;
     return Math.floor(multi / sum);
   };
 
-  getAllParks = async (guest) => {
-    if (!guest) {
-      let data = await $.get('/api/skateparks');
-      data.map((p) =>
-        p.rating ? (p.rating = this.calculateRating(p.rating)) : (p.rating = 0)
-      );
-      this._data.skateParks = data;
-    } else {
-      let data = await $.get('/api/skateparks/guest');
-      data.map((p) =>
-        p.rating ? (p.rating = this.calculateRating(p.rating)) : (p.rating = 0)
-      );
-      this._data.skateParks = data;
-    }
+  getAllParks = async guest => {
+    const query = guest ? '/api/skateparks/guest' : '/api/skateparks';
+    let data = await $.get(query);
+    this._data.skateParks = data.map(park => (park.rating ? (park.rating = this.calculateRating(park.rating)) : (park.rating = 0)));
   };
 
-  addPark = async (park) => {
+  addPark = async park => {
     const newPark = await $.post('/api/parks', park);
     this._data.skateParks.push(newPark);
   };
 
-  giveRating = async (rate) => {
+  giveRating = async rate => {
     await $.ajax({
       url: `api/parks/${rate}`,
-      type: 'PUT',
+      type: 'PUT'
     });
   };
 }
